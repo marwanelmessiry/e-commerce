afe6b9087bb61ffe442f24b2e9c1b43aca5d7c48
hdsg5610 <marwan.elmessiry@orange.com>
2025-02-03 01:03:01 +0200

    adding the global error handler to middleware directory

--- MiddleWares/errorMiddleware.js
+++ MiddleWares/errorMiddleware.js
@@ -3,11 +3,28 @@
 const globalError = (err, req, res, next) => {
     err.StatusCode = err.StatusCode || 500
     err.status = err.status || 'error'
+    if (process.env.NODE_ENV == 'development') {
+        senErrorForDev(err, res)
+
+    }
+    else if (process.env.NODE_ENV == 'production') {
+        senErrorForprod(err, res)
+
+    }
+}
+const senErrorForDev = (err, res) => {
     res.status(err.StatusCode).json({
         status: err.status,
         error: err,
         message: err.message,
         stack: err.stack,
+    })
+}
+const senErrorForprod = (err, res) => {
+    res.status(err.StatusCode).json({
+        status: err.status,
+        error: err,
+
     })
 }
 module.exports = globalError
