c3345ef29b60fd48b726f63798e58ae1bd634b97
hdsg5610 <marwan.elmessiry@orange.com>
2025-02-18 21:39:59 +0200

    adding validation layer to all categories CRUD operations

--- Routes/CatRoutes.js
+++ Routes/CatRoutes.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const { param, validationResult } = require('express-validator')
-const { getCategories, createCategory, updatecategory, getOneCategory, deleteCategory } = require("../services/catServices");
+const { getCategories,
+    createCategory,
+    updatecategory,
+    getOneCategory,
+    deleteCategory }
+    = require("../services/catServices");
+const { getCategoryValidator,
+    createCategoryValidator,
+    updateCategoryValidator,
+    deleteCategoryValidator
+} = require('../utils/validators/catValidator');
 const router = express.Router();
 
-router.route('/').get(getCategories).post(createCategory);
-router.route('/:id').get(param('id').isMongoId().withMessage('Invalid category id'), (req, res) => {
-    const result = validationResult(req);
-    if (result.isEmpty()) {
-        return res.status(400).send(`Error in ID as it is  ${req.param.id}!`);
-    }
-
-    res.status(400).send({ errors: result.array() });
-}, getOneCategory).put(updatecategory).delete(deleteCategory);
+router.route('/').get(getCategories).post(createCategoryValidator, createCategory);
+router.route('/:id').get(getCategoryValidator, getOneCategory).put(updateCategoryValidator, updatecategory).delete(deleteCategoryValidator, deleteCategory);
 module.exports = router
--- utils/validators/catValidator.js
+++ utils/validators/catValidator.js
@@ -4,3 +4,17 @@ exports.getCategoryValidator = [
     check('id').isMongoId().withMessage('Invalid category id'),
     validatorMiddleware
 ]
+exports.createCategoryValidator = [
+    check('name').notEmpty().withMessage('Category required')
+        .isLength({ min: 3 }).withMessage('Category is too short')
+        .isLength({ max: 32 }).withMessage('Category is too long'),
+    validatorMiddleware
+]
+exports.updateCategoryValidator = [
+    check('id').isMongoId().withMessage('Invalid category id'),
+    validatorMiddleware
+]
+exports.deleteCategoryValidator = [
+    check('id').isMongoId().withMessage('Invalid category id'),
+    validatorMiddleware
+]
